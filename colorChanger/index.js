a1eb2eebd70041f3deca2f9405b1a2877d4a86c7
Pushpam12 <126230025+Pushpam12@users.noreply.github.com>
2023-09-06 15:50:39 +0530

    Add files via upload

---
+++ colorChanger/index.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector(".btn");
+let box = document.querySelector('div.box');
+
+btn.addEventListener('click', () => {
+    box.style.backgroundColor = colorGenerator();
+})
+
+function colorGenerator() {
+    let arr = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+        color += arr[ Math.floor(Math.random()*16) ];
+    }
+    return '#'+ color;    
+}
+
+
+
+
+
+
